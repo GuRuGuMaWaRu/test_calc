e0b5b5e7d65487ad9078f873eeb3e3f9638e4d47
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-05 14:49:29 +0200

    looking for a new data structure

--- src/CalcButton.js
+++ src/CalcButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function CalcButton(props) {
   return (
-    <button onClick={props.onClick}>
+    <button onClick={() => props.onClick(props.value)}>
       {props.value}
     </button>
   );
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,71 +1,113 @@
 import React, { Component } from 'react';
 import CalcButton from './CalcButton';
+import DisplayResult from './DisplayResult';
 import './Calculator.css';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      input: [[11],[22]],
       firstNum: '',
       operator: '',
       secondNum: '',
+      result: '',
       buttons: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', '*', '/']
     };
   }
 
 
-  handleClick = event => {
-    let value = event.target.textContent;
+  handleClick = value => {
+    let container = this.state.input.length > 0 ? this.state.input.slice(-1) : [];
 
-    if (/\d/.test(value) && this.state.operator === '') {
-      this.setState(prevState => ({
-        firstNum: prevState.firstNum + value
-      }));
-    } else if (!/\d/.test(value) && this.state.firstNum.length > 0) {
-      this.setState({
-        operator: value
-      });
-    } else if (/\d/.test(value)) {
-      this.setState(prevState => ({
-        secondNum: prevState.secondNum + value
-      }));
+    //======== getting rid of a leading zero
+    if (container.length === 1 && container[0] === '0' && /\d/.test(value)) {
+      container[0] = value;
+    } else if (!/\d/.test(value) && this.state.input.length > 0) {
+      container = [value];
     }
+    // let value = event.target.textContent;
+
+    // this.setState(prevState => ({
+    //   input: prevState.input + value
+    // }));
+
+    // this.parseInput();
+  }
+
+  parseInput = () => {
+    // for (let i = 0, len = this.state.input.length; i < len; i++) {
+    //   let value = this.state.input[i];
+    //
+    //   if (/\d/.test(value) && this.state.operator === '') {
+    //     this.setState(prevState => ({
+    //       firstNum: prevState.firstNum + value
+    //     }));
+    //   } else if (!/\d/.test(value) && this.state.firstNum.length > 0) {
+    //     this.setState({
+    //       operator: value
+    //     });
+    //   } else if (/\d/.test(value)) {
+    //     this.setState(prevState => ({
+    //       secondNum: prevState.secondNum + value
+    //     }));
+    //     this.calculateResults();
+    //   } else if (!/\d/.test(value)) {
+    //     this.setState({
+    //       firstNum: this.state.result
+    //     });
+    //   }
+    // }
   }
 
   calculateResults = () => {
-    if (this.state.secondNum.length > 0) {
-      const firstNumber = Number.parseInt(this.state.firstNum, 10),
-            secondNumber = Number.parseInt(this.state.secondNum, 10);
-
-      switch(this.state.operator) {
-        case '+':
-          return firstNumber + secondNumber;
-        case '-':
-          return firstNumber - secondNumber;
-        case '*':
-          return firstNumber * secondNumber;
-        case '/':
-          return firstNumber / secondNumber;
-        default:
-          return '';
-      }
-    }
+    // if (this.state.secondNum.length > 0) {
+    //   const firstNumber = Number.parseInt(this.state.firstNum, 10),
+    //         secondNumber = Number.parseInt(this.state.secondNum, 10);
+    //
+    //   switch(this.state.operator) {
+    //     case '+':
+    //       this.setState({
+    //         result: firstNumber + secondNumber
+    //       });
+    //       break;
+    //     case '-':
+    //       this.setState({
+    //         result: firstNumber - secondNumber
+    //       });
+    //       break;
+    //     case '*':
+    //       this.setState({
+    //         result: firstNumber * secondNumber
+    //       });
+    //       break;
+    //     case '/':
+    //       this.setState({
+    //         result: firstNumber / secondNumber
+    //       });
+    //       break;
+    //     default:
+    //       this.setState({
+    //         result: ''
+    //       });
+    //   }
+    // }
   }
 
   handleDelete = () => {
-    if (this.state.secondNum.length > 0) {
-      this.setState(prevState => ({
-        secondNum: prevState.secondNum.slice(0, -1)
-      }));
-    } else if (this.state.operator.length > 0) {
-      this.setState({
-        operator: ''
-      });
-    } else if (this.state.firstNum.length > 0) {
-      this.setState(prevState => ({
-        firstNum: prevState.firstNum.slice(0, -1)
-      }));
-    }
+    // if (this.state.secondNum.length > 0) {
+    //   this.setState(prevState => ({
+    //     secondNum: prevState.secondNum.slice(0, -1)
+    //   }));
+    // } else if (this.state.operator.length > 0) {
+    //   this.setState({
+    //     operator: ''
+    //   });
+    // } else if (this.state.firstNum.length > 0) {
+    //   this.setState(prevState => ({
+    //     firstNum: prevState.firstNum.slice(0, -1)
+    //   }));
+    // }
   }
 
   display = () => {
@@ -83,9 +125,7 @@ class Calculator extends Component {
           <div className="Display">
             {this.state.firstNum + this.state.operator + this.state.secondNum}
           </div>
-          <div className="Results">
-            {this.calculateResults()}
-          </div>
+          <DisplayResult input={this.state.input} className="Results" />
         </div>
         <div>
           <h4>Buttons</h4>
