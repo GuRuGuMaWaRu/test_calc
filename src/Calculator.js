19e1460a30f6f54c8d69baf9b56b0de6b23b58ac
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-10 18:41:54 +0200

    add basic calculations

--- src/Calculator.js
+++ src/Calculator.js
@@ -16,10 +16,10 @@ export default class Calculator extends Component {
       input: [],
       last: '1',
       decimalDot: false,
-      // firstNum: '',
+      // firstNumber: '',
       // operator: '',
-      // secondNum: '',
-      // result: '',
+      // secondNumber: '',
+      result: '',
       buttons: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', '*', '/', '.']
     };
   }
@@ -41,37 +41,26 @@ export default class Calculator extends Component {
       let container = input.length > 0 && /\d|\./.test(this.state.last) ? input.pop() : '';
 
       if (container.length === 0 && /\./.test(value)) { //=== deal with leading decimal dot issue
-        console.log('deal with leading decimal dot issue');
         container = '0.';
         this.setState({
           decimalDot: true
         });
         this.updateState(value, container, input);
       } else if (container.length === 1 && container === '0') { //=== deal with leading zero issue
-        console.log('deal with leading zero issue');
         container = value.toString();
         this.updateState(value, container, input);
       } else if (/\./.test(value)) { //=== add decimal dot only if there is none
-        console.log('add decimal dot if there is none');
         container += value;
         this.setState({
           decimalDot: true
         });
         this.updateState(value, container, input);
       } else if (!/\./.test(value)) { //=== add anything except a decimal dot
-        console.log('add anything except a decimal dot');
         container += value;
         this.updateState(value, container, input);
       }
-      // input.push(container);
-      // this.setState({
-      //   input: input,
-      //   last: value
-      // });
     } else if (!/\d|\./.test(value) && input.length > 0) { //=== add operator
-      console.log('add operator');
       if (!/\d/.test(this.state.last)) { //=== ensure there is only one operator
-        console.log('ensure there is only one operator');
         input.pop();
         input.push(value);
       } else {
@@ -83,65 +72,68 @@ export default class Calculator extends Component {
         decimalDot: false
       });
     }
+    this.parseInput();
   }
 
   parseInput = () => {
-    // for (let i = 0, len = this.state.input.length; i < len; i++) {
-    //   let value = this.state.input[i];
-    //
-    //   if (/\d/.test(value) && this.state.operator === '') {
-    //     this.setState(prevState => ({
-    //       firstNum: prevState.firstNum + value
-    //     }));
-    //   } else if (!/\d/.test(value) && this.state.firstNum.length > 0) {
-    //     this.setState({
-    //       operator: value
-    //     });
-    //   } else if (/\d/.test(value)) {
-    //     this.setState(prevState => ({
-    //       secondNum: prevState.secondNum + value
-    //     }));
-    //     this.calculateResults();
-    //   } else if (!/\d/.test(value)) {
-    //     this.setState({
-    //       firstNum: this.state.result
-    //     });
-    //   }
-    // }
+    let input = this.state.input,
+        firstNumber = '',
+        operator = '',
+        secondNumber = '',
+        result = '';
+
+    for (let i = 0, len = input.length; i < len; i++) {
+
+      if (/\d/.test(input[i]) && firstNumber.length === 0) {
+        firstNumber = input[i];
+      } else if (!/\d/.test(input[i])) {
+        operator = input[i];
+      } else if (/\d/.test(input[i]) && secondNumber.length === 0) {
+        secondNumber = input[i];
+      }
+
+      if (secondNumber.length > 0) {
+        result = this.calculate(firstNumber, operator, secondNumber);
+        firstNumber = result;
+        operator = '';
+        secondNumber = '';
+        this.setState({
+          result: result
+        });
+      }
+
+      // if (/\d/.test(this.state.input[i]) && this.state.firstNumber.length === 0) {
+      //   this.setState({
+      //     firstNumber: this.state.input[i]
+      //   });
+      // } else if (!/\d/.test(this.state.input[i])) {
+      //   this.setState({
+      //     operator: this.state.input[i]
+      //   });
+      // } else if (/\d/.test(this.state.input[i]) && this.state.secondNumber.length === 0) {
+      //   this.setState({
+      //     secondNumber: this.state.input[i]
+      //   });
+      // }
+    }
   }
 
-  calculateResults = () => {
-    // if (this.state.secondNum.length > 0) {
-    //   const firstNumber = Number.parseInt(this.state.firstNum, 10),
-    //         secondNumber = Number.parseInt(this.state.secondNum, 10);
-    //
-    //   switch(this.state.operator) {
-    //     case '+':
-    //       this.setState({
-    //         result: firstNumber + secondNumber
-    //       });
-    //       break;
-    //     case '-':
-    //       this.setState({
-    //         result: firstNumber - secondNumber
-    //       });
-    //       break;
-    //     case '*':
-    //       this.setState({
-    //         result: firstNumber * secondNumber
-    //       });
-    //       break;
-    //     case '/':
-    //       this.setState({
-    //         result: firstNumber / secondNumber
-    //       });
-    //       break;
-    //     default:
-    //       this.setState({
-    //         result: ''
-    //       });
-    //   }
-    // }
+  calculate = (firstNumber, operator, secondNumber) => {
+    firstNumber = Number.parseInt(firstNumber, 10);
+    secondNumber = Number.parseInt(secondNumber, 10);
+    console.log('calculate!');
+    switch(operator) {
+      case '+':
+        return firstNumber + secondNumber;
+      case '-':
+        return firstNumber - secondNumber;
+      case '*':
+        return firstNumber * secondNumber;
+      case '/':
+        return firstNumber / secondNumber;
+      default:
+        return '';
+    }
   }
 
   handleDelete = () => {
@@ -177,6 +169,9 @@ export default class Calculator extends Component {
           <div className="Display">
             {/* {this.state.firstNum + this.state.operator + this.state.secondNum} */}
             {this.state.input}
+          </div>
+          <div className="Result">
+            {this.state.result}
           </div>
         </div>
         <div>
