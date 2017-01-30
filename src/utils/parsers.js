84c20107182926598853bd264eaefeddfd62bd9a
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-30 14:22:34 +0200

    Cosmetic changes - missing semicolons, separate variable declarations

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,7 +1,8 @@
-const parseInput = (input) => {
-  let match = input.replace(/\b0{2,}/, ''); //=== remove leading zeroes
-  match = match.replace(/^\./, '0.');
-  console.log(match);
+const parseInput = (input) =>
+  let match = '';
+  match = input.replace(/\b0{2,}/, ''); //=== remove leading zeroes
+  match = match.replace(/^\./, '0.'); //=== insert zero before leading decimal dot
+  match = match.replace();
   return match;
 }
 
