bed235e03209d71891112641b35e30aefb80ca7d
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-30 16:09:55 +0200

    Use another regex to solve duplicate decimal point issue

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,7 +1,7 @@
 const parseInput = (input) => {
   let match = '';
   match = input.replace(/\b0([0-9])/, '$1'); //=== remove leading zeroes
-  match = match.replace(/(^|[+-/*])(\.)/, '$10$2'); //=== insert zero before leading decimal dot
+  match = match.replace(/(^|\D)(\.)/, '$10$2'); //=== insert zero before leading decimal dot
   // match = match.replace(/\b(\d+\.)(\d+)?(\.)/, '$1$2'); //=== solve duplicate decimal dot issue
   return match;
 }
