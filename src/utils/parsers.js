9373ae8b3f99ba71a1ac5253b145c8849c1f08be
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-30 14:58:15 +0200

    Correct solution for the leading zero issue

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,6 +1,6 @@
 const parseInput = (input) => {
   let match = '';
-  match = input.replace(/\b0{2,}/, ''); //=== remove leading zeroes
+  match = input.replace(/\b0([0-9])/, '$1'); //=== remove leading zeroes
   match = match.replace(/^\./, '0.'); //=== insert zero before leading decimal dot
   match = match.replace();
   return match;
