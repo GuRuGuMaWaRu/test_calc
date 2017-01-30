52656b93fa8fe931913165d1a6a1a163686d2c5b
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-30 15:49:41 +0200

    Update tests

--- src/tests/parsers.test.js
+++ src/tests/parsers.test.js
@@ -7,8 +7,10 @@ it('removes redundant leading zeroes', () => {
 
 it('inserts zero before the leading decimal dot', () => {
   expect(parseInput('.')).toEqual('0.');
+  expect(parseInput('6563+0.34/.')).toEqual('6563+0.34/0.');
 });
 
 it('allows only one decimal dot per number', () => {
   expect(parseInput('0.34.').toEqual('0.34'));
+  expect(parseInput('6563+0.34/434..').toEqual('6563+0.34/434.'));
 });
--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -2,7 +2,7 @@ const parseInput = (input) => {
   let match = '';
   match = input.replace(/\b0([0-9])/, '$1'); //=== remove leading zeroes
   match = match.replace(/(^|[+-/*])(\.)/, '$10$2'); //=== insert zero before leading decimal dot
-  // match = match.match(/(^|[+-/*]|\b)\d+(\.)?(\d+|[+-/*])/); //=== solve duplicate decimal dot issue
+  // match = match.replace(/\b(\d+\.)(\d+)?(\.)/, '$1$2'); //=== solve duplicate decimal dot issue
   return match;
 }
 
