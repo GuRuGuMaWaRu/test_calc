8e42ef6607bdacb0f55076ccc60ae1c108e7130e
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-31 09:44:58 +0200

    Solve consecutive operators issue

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -4,6 +4,7 @@ const parseInput = (input) => {
   match = match.replace(/(^|[\/\+\-\*])(\.)/, '$10$2'); //=== insert zero before leading decimal dot
   match = match.replace(/\b(\d+\.)(\d+)?(\.)/, '$1$2'); //=== solve duplicate decimal dot issue
   match = match.replace(/^[\/\+\-\*]/, ''); //=== solve leading operator issue
+  match = match.replace(/[\/\+\-\*](?=[\/\+\-\*])/, ''); //=== solve consecutive operators issue
   return match;
 }
 
