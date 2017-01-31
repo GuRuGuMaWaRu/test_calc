8f885f652e9237a90f73c01494d649f036b0f530
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-31 09:27:30 +0200

    Group first tests into 'raw input parser' test suite

--- src/tests/parsers.test.js
+++ src/tests/parsers.test.js
@@ -1,16 +1,19 @@
 import parseInput from '../utils/parsers';
 
-it('removes redundant leading zeroes', () => {
-  expect(parseInput('00')).toEqual('0');
-  expect(parseInput('06')).toEqual('6');
-});
+describe('raw input parser', () => {
+  it('removes redundant leading zeroes', () => {
+    expect(parseInput('00')).toEqual('0');
+    expect(parseInput('06')).toEqual('6');
+  });
 
-it('inserts zero before the leading decimal dot', () => {
-  expect(parseInput('.')).toEqual('0.');
-  expect(parseInput('6563+0.34/.')).toEqual('6563+0.34/0.');
-});
+  it('inserts zero before the leading decimal dot', () => {
+    expect(parseInput('.')).toEqual('0.');
+    expect(parseInput('6563+0.34/.')).toEqual('6563+0.34/0.');
+  });
 
-it('allows only one decimal dot per number', () => {
-  expect(parseInput('0.34.')).toEqual('0.34');
-  expect(parseInput('6563+0.34/434..')).toEqual('6563+0.34/434.');
+  it('allows only one decimal dot per number', () => {
+    expect(parseInput('0.34.')).toEqual('0.34');
+    expect(parseInput('6563+0.34/434..')).toEqual('6563+0.34/434.');
+  });
+
 });
