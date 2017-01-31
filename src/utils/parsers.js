5dd69e51fe82a9b7998e1ee32b3f7ed867ce0072
Peter Krevenets <peter.k.nets@gmail.com>
2017-01-31 09:34:40 +0200

    Solve leading operator issue

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -3,6 +3,7 @@ const parseInput = (input) => {
   match = input.replace(/\b0([0-9])/, '$1'); //=== solve leading zero issue
   match = match.replace(/(^|[\/\+\-\*])(\.)/, '$10$2'); //=== insert zero before leading decimal dot
   match = match.replace(/\b(\d+\.)(\d+)?(\.)/, '$1$2'); //=== solve duplicate decimal dot issue
+  match = match.replace(/^[\/\+\-\*]/, ''); //=== solve leading operator issue
   return match;
 }
 
